d848be69af5a370772dc105253e1115f119e4da8
Mukhammadjon Sirojiddinov <mukhammadjon.707s@gmail.com>
2023-05-25 13:47:55 +0500

    Tests has been added for user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+  const createUserDto: CreateUserDto = {
+    name: 'Name',
+    address: 'Address',
+    description: 'Description',
+    dob: new Date('2023-05-25T08:22:03.948Z'),
+  };
+
+  const mockUser = {
+    name: 'Name',
+    address: 'Address',
+    description: 'Description',
+    dob: new Date('2023-05-25T08:22:03.948Z'),
+    _id: 'a id',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                name: 'Name 1',
+                address: 'Address 1',
+                description: 'Description 1',
+                dob: '2023-05-25T08:22:03.948Z',
+              },
+              {
+                name: 'Name 2',
+                address: 'Address 2',
+                description: 'Description 2',
+                dob: '2023-05-25T08:22:03.948Z',
+              },
+              {
+                name: 'Name 3',
+                address: 'Address 3',
+                description: 'Description 3',
+                dob: '2023-05-25T08:22:03.948Z',
+              },
+            ]),
+            create: jest.fn().mockResolvedValue(createUserDto),
+            findOne: jest.fn().mockResolvedValue(mockUser),
+            update: jest
+              .fn()
+              .mockRejectedValue({ ...createUserDto, name: 'updated name' }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all users', async () => {
+    expect(controller.findAll()).resolves.toEqual([
+      {
+        name: 'Name 1',
+        address: 'Address 1',
+        description: 'Description 1',
+        dob: '2023-05-25T08:22:03.948Z',
+      },
+      {
+        name: 'Name 2',
+        address: 'Address 2',
+        description: 'Description 2',
+        dob: '2023-05-25T08:22:03.948Z',
+      },
+      {
+        name: 'Name 3',
+        address: 'Address 3',
+        description: 'Description 3',
+        dob: '2023-05-25T08:22:03.948Z',
+      },
+    ]);
+
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('add new user', async () => {
+    const createSpy = jest
+      .spyOn(service, 'create')
+      .mockResolvedValueOnce(mockUser);
+
+    await controller.create(createUserDto);
+    expect(createSpy).toHaveBeenCalledWith(createUserDto);
+  });
+
+  it('should return user by id', async () => {
+    expect(controller.findOne('1')).resolves.toEqual({
+      name: 'Name',
+      address: 'Address',
+      description: 'Description',
+      dob: new Date('2023-05-25T08:22:03.948Z'),
+      _id: 'a id',
+    });
+
+    expect(service.findOne).toHaveBeenCalled();
+  });
+
+  it('should update data', async () => {
+    const userDataToUpdate = { ...createUserDto, name: 'updated name' };
+    const updateSpy = jest
+      .spyOn(service, 'update')
+      .mockResolvedValueOnce(userDataToUpdate);
+
+    await controller.update('a _id', userDataToUpdate);
+    expect(updateSpy).toHaveBeenCalledWith('a _id', userDataToUpdate);
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+
+const mockUser = {
+  name: 'Name',
+  address: 'Address',
+  description: 'Description',
+  dob: new Date('2023-05-25T08:22:03.948Z'),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let model: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            new: jest.fn().mockResolvedValue({}),
+            constructor: jest.fn().mockResolvedValue({}),
+            find: jest.fn().mockReturnValue({
+              exec: jest.fn().mockResolvedValue([
+                {
+                  name: 'Name 1',
+                  address: 'Address 1',
+                  description: 'Description 1',
+                  dob: '2023-05-25T08:22:03.948Z',
+                },
+                {
+                  name: 'Name 2',
+                  address: 'Address 2',
+                  description: 'Description 2',
+                  dob: '2023-05-25T08:22:03.948Z',
+                },
+              ]),
+            }),
+            create: jest.fn().mockImplementationOnce(() =>
+              Promise.resolve({
+                name: 'Name',
+                address: 'Address',
+                description: 'Description',
+                dob: new Date('2023-05-25T08:22:03.948Z'),
+              }),
+            ),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    model = module.get<Model<User>>(getModelToken('User'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all users', async () => {
+    const result = await service.findAll();
+    expect(result.length).toEqual(2);
+  });
+
+  it('should add new user', async () => {
+    const newUser = await service.create({
+      name: 'Name',
+      address: 'Address',
+      description: 'Description',
+      dob: new Date('2023-05-25T08:22:03.948Z'),
+    });
+
+    expect(newUser).toEqual(mockUser);
   });
 });

223cc2e3216847570ad6c1d872a1d8312a50df3e
Mukhammadjon Sirojiddinov <mukhammadjon.707s@gmail.com>
2023-05-25 10:59:04 +0500

    Configured database

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Application is runnning on ${await app.getUrl()}`);
 }
 bootstrap();
